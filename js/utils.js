96247bf403d939879178af4f1db591d0d9d33e2a
Damian Norman <damiannorman@gmail.com>
2019-08-22 20:45:08 -0400

    initial

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+function getTODO() {
+	return fetch('https://jsonplaceholder.typicode.com/todos/1')
+		.then(response => {
+			return response.json();
+		});
+};
+
+function getArticle() {
+	return fetch('http://mtrest.advance.net/mtrest/articles/?blog_id=3674&limit=10&offset=0')
+		.then(response => {
+			return response.json();
+		});
+};
